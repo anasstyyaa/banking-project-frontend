0ee3fd04343b08aae2b5c70ec77c2b2c6c335d44
Micaela <micalambrogioi@gmail.com>
2026-05-06 12:22:37 +0200

    Revert "Merge branch 'main' of https://github.com/anasstyyaa/banking-project-frontend"
    This reverts commit 4a5d4f490e19eb348d5cbc16f8ee1bd603093113, reversing
    changes made to d6d94c2baf844b264cf580a055043c7f55b5e398.

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,15 @@
-/// <reference types="vitest/config" />
 import { fileURLToPath, URL } from 'node:url'
-import path from 'node:path'
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin'
-import { playwright } from '@vitest/browser-playwright'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const dirname =
-  typeof __dirname !== 'undefined'
-    ? __dirname
-    : path.dirname(fileURLToPath(import.meta.url))
-
 export default defineConfig({
+  plugins: [
+    vue(),
+  ],
   base: '/banking-project-frontend/',
-  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-  },
-  test: {
-    projects: [
-      {
-        extends: true,
-        plugins: [
-          storybookTest({
-            configDir: path.join(dirname, '.storybook'),
-          }),
-        ],
-        test: {
-          name: 'storybook',
-          browser: {
-            enabled: true,
-            headless: true,
-            provider: playwright({}),
-            instances: [
-              {
-                browser: 'chromium',
-              },
-            ],
-          },
-        },
-      },
-    ],
   },
 })
